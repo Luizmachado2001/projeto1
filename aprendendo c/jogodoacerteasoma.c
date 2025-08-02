718d98958e769dddc45e971a6f6db98b6aea5303
= <luizmachado20010@gmail.com>
2025-08-02 18:30:56 -0300

    new game

---
+++ aprendendo c/jogodoacerteasoma.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int num1, num2, result, resp;
+    
+    // receive number 1
+    printf("enter number: ");
+    scanf("%i", &num1);
+
+    // receive number 2
+    printf("enter number: ");
+    scanf("%i", &num2);
+    
+    // verification the result
+    result = num1 + num2;
+
+    printf("What is the correct sum? : ");
+
+    return 0;
+}
